291a99ffa2468118b31488e6917c0f86fd9198a4
Manoj Subramanyam <manojsubramanyam.ol@gmail.com>
2017-03-27 19:13:02 +0530

    Update and rename code4.R to TrainingTestingSplits.R

--- TrainingTestingSplits.R
+++ TrainingTestingSplits.R
@@ -11,15 +11,15 @@ library(caTools) # for sampling
 # loading data structures of wine
 load("DataWine3.R")
 set.seed(123)
-spl= sample.split(wine_norm$quality, SplitRatio = 0.8)
+spl <- sample.split(wine_norm$quality, SplitRatio = 0.8)
 
 # Train and test sets for Unnormalized data set wine_imputed
-wine_train= subset(wine_imputed, spl== T)
-wine_test= subset(wine_imputed, spl== F)
+wine_train <- subset(wine_imputed, spl== T)
+wine_test <- subset(wine_imputed, spl== F)
 
 # Train and test sets for Normalized data set wine_norm
-wine_norm_train= subset(wine_norm, spl== T)
-wine_norm_test= subset(wine_norm, spl== F)
+wine_norm_train <- subset(wine_norm, spl== T)
+wine_norm_test <- subset(wine_norm, spl== F)
 
 
 # saving data structures of training and test data sets for further usuage
